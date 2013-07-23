1709557bda6edfa0903b67d89f7fb4b2cb9a8135
Ubuntu <ubuntu@ip-172-31-17-13.us-west-2.compute.internal>
2013-07-23 19:47:16 +0000

    update web.js & index.html

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname + '/'));
 app.get('/', function(request, response) {
 var fs=require('fs');
 var content=fs.readFileSync('index.html'); 
